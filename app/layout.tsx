94dda3bfcc69a260632a7a405cc3bd7ae51aa05a
bom <bibidibovidiboo@gmail.com>
2024-02-19 01:09:40 +0900

    Add cover image file upload function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from 'sonner';
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next';
 
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ConvexClientProvider } from '@/components/providers/convex-provider';
 import { ModalProvider } from '@/components/providers/modal-provider';
+import { EdgeStoreProvider } from '@/lib/edgestore';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -34,20 +36,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
-            enableSystem
-            disableTransitionOnChange
-            storageKey='bomion-theme-2'
-          >
-            <Toaster position='bottom-center' />
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='system'
+              enableSystem
+              disableTransitionOnChange
+              storageKey='bomion-theme-2'
+            >
+              <Toaster position='bottom-center' />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
